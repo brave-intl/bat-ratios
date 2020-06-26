99122b43b30a4a2e70de82c9aea4f3241ee5af15
Michael McLaughlin <4896982+maikelmclauflin@users.noreply.github.com>
2020-06-26 15:32:18 -0500

    updated deps and granted permissive regexp (#57)
    * updated deps and granted permissive regexp
    
    * removed uncomment

--- versions/regexp.js
+++ versions/regexp.js
@@ -1,7 +1,7 @@
 module.exports = {
   intOrDecimal: /\d*\.?\d+/i,
-  numberWithUnit: /^-?[0-9A-Z]{2,}$/i,
-  dashNumberWithUnit: /^[0-9A-Z]{2,}-[0-9A-Z]{2,}$/i,
+  numberWithUnit: /.+/i,
+  dashNumberWithUnit: /.+/i,
   symbol: /^[A-Z]{3}$/i,
   dualSymbolNumbers: /^[0-9A-Z]{2,}[0-9A-Z]{2,}$/i
 }
