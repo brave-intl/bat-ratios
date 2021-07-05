716b3663deb6002deaa568bdf57432657ca0e8c1
Michael McLaughlin <4896982+maikelmclauflin@users.noreply.github.com>
2021-07-05 11:57:38 -0500

    added 24hr change for currencies (#85)
    * changed ports for ratios dbs and added usd_24h_change for relative url
    
    * found that alt currencies must be used with usd or other base currencies
    
    * removed log

--- test/v1/index.test.js
+++ test/v1/index.test.js
@@ -622,19 +622,27 @@ test('check coingecko results where the second timestamp defaults to now', async
 })
 
 test('check coingecko spot price', async (t) => {
-  const url = '/v2/relative/provider/coingecko/basic-attention-token/usd'
-  const { body } = await ratiosAgent
-    .get(url)
-    .expect(ok)
-  const { usd } = body.payload['basic-attention-token']
-  t.deepEqual(body, {
-    lastUpdated: body.lastUpdated,
-    payload: {
-      'basic-attention-token': {
-        usd
-      }
+  const checkAgainstCurrency = async (ca, cb) => {
+    const url = `/v2/relative/provider/coingecko/${ca}/${cb}`
+    const { body } = await ratiosAgent
+      .get(url)
+      .expect(ok)
+    const { payload } = body
+    const change24Key = `${cb}_24h_change`
+    const a = {
+      [cb]: payload[ca][cb],
+      [change24Key]: payload[ca][change24Key]
     }
-  })
+    t.true(!_.isNaN(new Date(body.lastUpdated).valueOf()))
+    t.deepEqual(body, {
+      lastUpdated: body.lastUpdated,
+      payload: {
+        [ca]: a
+      }
+    })
+  }
+  await checkAgainstCurrency('basic-attention-token', 'usd')
+  await checkAgainstCurrency('bitcoin', 'usd')
 })
 
 test('check coingecko spot price with mapped ticker', async (t) => {
@@ -642,12 +650,17 @@ test('check coingecko spot price with mapped ticker', async (t) => {
   const { body } = await ratiosAgent
     .get(url)
     .expect(ok)
-  const { usd } = body.payload.bat
+  const { payload } = body
+  t.true(!_.isNaN(new Date(body.lastUpdated).valueOf()))
+  const bat = {
+    usd: payload.bat.usd,
+    usd_24h_change: payload.bat.usd_24h_change
+  }
   t.deepEqual(body, {
     lastUpdated: body.lastUpdated,
     payload: {
-      'basic-attention-token': { usd },
-      bat: { usd }
+      'basic-attention-token': bat,
+      bat
     }
   })
 })
