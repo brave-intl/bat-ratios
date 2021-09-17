80bddd2eed6ab469eea25ae3a7b7a83dd0538856
husobee <husobee@e-ie.io>
2021-09-17 09:16:25 -0400

    adding a passthrough for etherscan api

---
+++ versions/v2/etherscan.js
@@ -0,0 +1,38 @@
+const env = require('$/env')
+const querystring = require('querystring')
+const currency = require('$/versions/currency')
+const Cache = require('$/versions/cache')
+const cache = Cache.create(30, {
+  url: env.REDIS_URL
+})
+
+module.exports = {
+  passthrough
+}
+
+// second argument is a parsed query string
+function passthrough (notvar, {
+  refresh,
+  path: basePath,
+  query
+}) {
+  let qs = ''
+  const base = {
+    module: 'gastracker',
+    action: 'gasoracle'
+  }
+  if (env.ETHERSCAN_APIKEY) {
+    base.apikey = env.ETHERSCAN_APIKEY
+  }
+  qs = `?${querystring.stringify(Object.assign(base, query))}`
+  if (basePath === undefined) {
+    basePath = '/api'
+  }
+  const path = `${basePath}${qs}`
+  return cache(path, () => currency.request({
+    hostname: 'api.etherscan.io',
+    protocol: 'https:',
+    path,
+    method: 'GET'
+  }), refresh)
+}
